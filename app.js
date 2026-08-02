199445f51f28bce8d4e68a94465aa072cf3bc55b
Adrian Mendoza Cruz <mendozastudios23@gmail.com>
2026-08-02 10:41:59 -0600

    Estructura inicial completa del proyecto AppsComputer

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ const env = require("dotenv").config()
 const port = process.env.PORT
 const app = express()
 const path = require("path")
+const fs = require('fs');
+
+//Inicio 
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/home', (req, res) => {
-   res.sendFile('index.html', {
-      root: __dirname
-   })
+   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.get('/office', (req, res) => {
-   res.sendFile('office.html', {
-      root: __dirname
-   })
+   res.sendFile(path.join(__dirname, 'office.html'))
 })
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'index.html', 'office.html')))
+app.get('/Microsoft-365-C2R-Installers', (req, res) => {
+   res.sendFile(path.join(__dirname, 'office-m365.html'))
+})
 
 app.listen(port, () => {
    console.log(`Servidor corriendo en "http://localhost:${port}/home"`)
